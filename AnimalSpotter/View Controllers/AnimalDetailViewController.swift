324dcc2775aeb03bfb414ca8da0764603d4d4e4f
John Kouris <jdkouris@gmail.com>
2019-09-11 19:39:31 -0700

    Created AnimalDetail and added necessary outlets

---
+++ AnimalSpotter/View Controllers/AnimalDetailViewController.swift
@@ -0,0 +1,26 @@
+//
+//  AnimalDetailViewController.swift
+//  AnimalSpotter
+//
+//  Created by John Kouris on 9/11/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class AnimalDetailViewController: UIViewController {
+    
+    @IBOutlet weak var spottedOnLabel: UILabel!
+    @IBOutlet weak var coordinatesLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var animalImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    
+}
